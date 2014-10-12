f1706fc4c1f640c4a0d7285393f646b1d039ea97
Stefan Birkner <mail@stefan-birkner.de>
2014-10-12 13:59:08 +0200

    Add a filter that disables encodeUrl.
    There are web sites that don't want to publish URLs with jsessonid.

---
+++ src/main/java/com/github/stefanbirkner/filtertools/filter/http/DisableEncodeUrlFilter.java
@@ -0,0 +1,56 @@
+package com.github.stefanbirkner.filtertools.filter.http;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponseWrapper;
+import java.io.IOException;
+
+/**
+ * Disables the methods
+ * {@link javax.servlet.http.HttpServletResponse#encodeURL(String)} and
+ * {@link javax.servlet.http.HttpServletResponse#encodeRedirectURL(String)}
+ * (as well as the deprecated methods
+ * {@link javax.servlet.http.HttpServletResponse#encodeUrl(String)} and
+ * {@link javax.servlet.http.HttpServletResponse#encodeRedirectUrl(String)}.
+ * These methods will return the URL unmodified if the
+ * {@code DisableEncodeUrlFilter} is active.
+ * <p><em>Caution!</em> This filter disables session support for clients
+ * that don't accept cookies, because the session id is no longer
+ * added to URLs.
+ */
+public class DisableEncodeUrlFilter extends HttpFilter {
+    @Override
+    protected void doFilter(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
+            throws IOException, ServletException {
+        DisableEncodeUrlResponse disableEncodeUrlResponse = new DisableEncodeUrlResponse(response);
+        filterChain.doFilter(request, disableEncodeUrlResponse);
+    }
+
+    private static class DisableEncodeUrlResponse extends HttpServletResponseWrapper {
+        public DisableEncodeUrlResponse(HttpServletResponse response) {
+            super(response);
+        }
+
+        @Override
+        public String encodeURL(String url) {
+            return url;
+        }
+
+        @Override
+        public String encodeRedirectURL(String url) {
+            return url;
+        }
+
+        @Override
+        public String encodeUrl(String url) {
+            return url;
+        }
+
+        @Override
+        public String encodeRedirectUrl(String url) {
+            return url;
+        }
+    }
+}
---
+++ src/test/java/com/github/stefanbirkner/filtertools/filter/http/DisableEncodeUrlFilterTest.java
@@ -0,0 +1,84 @@
+package com.github.stefanbirkner.filtertools.filter.http;
+
+import org.junit.Test;
+
+import javax.servlet.*;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.is;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class DisableEncodeUrlFilterTest {
+    private static final HttpServletRequest request = mock(HttpServletRequest.class);
+    private static final HttpServletResponse response = mock(HttpServletResponse.class);
+    private final Filter filter = new DisableEncodeUrlFilter();
+
+    @Test
+    @SuppressWarnings("deprecation")
+    public void disablesEncodeUrl() throws Exception {
+        when(response.encodeUrl(anyString())).thenReturn("encoded url");
+        assertThatUrlIsNotEncoded(new ResponseAction() {
+            @Override
+            public String callMethodOfResponse(HttpServletResponse response) {
+                return response.encodeUrl("not encoded url");
+            }
+        });
+    }
+
+    @Test
+    public void disablesEncodeURL() throws Exception {
+        when(response.encodeURL(anyString())).thenReturn("encoded url");
+        assertThatUrlIsNotEncoded(new ResponseAction() {
+            @Override
+            public String callMethodOfResponse(HttpServletResponse response) {
+                return response.encodeURL("not encoded url");
+            }
+        });
+    }
+
+    @Test
+    @SuppressWarnings("deprecation")
+    public void disablesEncodeRedirectUrl() throws Exception {
+        when(response.encodeRedirectUrl(anyString())).thenReturn("encoded url");
+        assertThatUrlIsNotEncoded(new ResponseAction() {
+            @Override
+            public String callMethodOfResponse(HttpServletResponse response) {
+                return response.encodeRedirectUrl("not encoded url");
+            }
+        });
+    }
+
+    @Test
+    @SuppressWarnings("deprecation")
+    public void disablesEncodeRedirectURL() throws Exception {
+        when(response.encodeRedirectURL(anyString())).thenReturn("encoded url");
+        assertThatUrlIsNotEncoded(new ResponseAction() {
+            @Override
+            public String callMethodOfResponse(HttpServletResponse response) {
+                return response.encodeRedirectURL("not encoded url");
+            }
+        });
+    }
+
+    private void assertThatUrlIsNotEncoded(final ResponseAction action)
+            throws IOException, ServletException {
+        filter.doFilter(request, response, new FilterChain() {
+            @Override
+            public void doFilter(ServletRequest request, ServletResponse response)
+                    throws IOException, ServletException {
+                String url = action.callMethodOfResponse((HttpServletResponse) response);
+                assertThat(url, is(equalTo("not encoded url")));
+            }
+        });
+    }
+
+    private static interface ResponseAction {
+        String callMethodOfResponse(HttpServletResponse response);
+    }
+}
